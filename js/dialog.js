9e771db96a17960e548a05a04427283629dc436a
juangf <jgarciafernandez@gmail.com>
2015-06-13 13:15:27 +0200

    Dialog container with flex-box.

--- js/dialog.js
+++ js/dialog.js
@@ -1,4 +1,5 @@
 _w.esp.dialog = {
+	_queue : [],	
 	_isOpen : false,
 	_showBackground : function(){
 		var bodyTag = _d.getElementsByTagName('body')[0],
@@ -26,19 +27,77 @@ _w.esp.dialog = {
 		return this;
 	},
 
-	show : function(){
+	_showDialogWrapper : function(){
+		var bodyTag = _d.getElementsByTagName('body')[0],
+			dialogWrapper = _d.createElement('esp-dialog-wrapper');
+
+		bodyTag.appendChild(dialogWrapper);
+
+		return this;
+	},
+
+	_showDialog : function(){
+		var bodyTag = _d.getElementsByTagName('body')[0],
+			dialogWrapper = _d.createElement('esp-dialog-wrapper'),
+			dialog = _d.createElement('esp-dialog');
+
+		dialogWrapper.appendChild(dialog);
+		bodyTag.appendChild(dialogWrapper);
+
+		setTimeout(function(){
+			dialogWrapper.classList.add('in');
+		},0);
+
+		return this;
+	},
+
+	_hideDialog : function(){
+		var bodyTag = _d.getElementsByTagName('body')[0],
+			dialogWrapper = _d.getElementsByTagName('esp-dialog-wrapper')[0];
+
+		_w.esp.one(dialogWrapper, 'webkitTransitionEnd transitionEnd', function(event){
+			bodyTag.removeChild(dialogWrapper);
+		});
+
+		dialogWrapper.classList.remove('in');
+
+		return this;
+	},
+
+	show : function(params){
+		/*
+			Parameters Object properties:
+			title - mandatory
+			content - mandatory
+
+		*/
+
+		//If is not displaying a dialog
 		if(!this._isOpen){
 			this._showBackground();
 			this._isOpen = true;
-		}		
+
+			this
+			._showDialog();
+
+
+		//If yes, add the new dialog to the queue
+		}else{
+			this._queue.push(params);
+		}
 
 		return this;
 	},
 
 	hide : function(){
 		if(this._isOpen){
-			this._hideBackground();
-			this._isOpen = false;
+			
+			this._hideDialog();	
+
+			if(this._queue.length < 1){				
+				this._hideBackground();
+				this._isOpen = false;
+			}
 		}
 		return this;
 	}
