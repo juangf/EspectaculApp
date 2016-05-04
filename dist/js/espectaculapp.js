adefabdd48b40d67d3872dfd9998333e7961d273
Juan <juan@snackson.com>
2016-05-04 18:17:35 +0200

    Added FontAwesome. Latest pull-to-refresh changes

--- dist/js/espectaculapp.js
+++ dist/js/espectaculapp.js
@@ -887,20 +887,24 @@ function AppView(name){
 
 							var ptrLoadingBox = _d.createElement('esp-list-ptr-loading-box');
 
-							ptrLoadingBox.innerText = 'Pull to refresh';
+							ptrLoadingBox.innerHTML = '<i class="fa fa-arrow-down"></i>';
 
 							list.insertBefore(ptrLoadingBox, list.firstChild);
 
 							list.addEventListener("touchstart", function(e){
 								if(list.scrollTop===0){
 									
+									//Reset the native scrolling
+									_w.scrollTo(0,0);
+
 									that._pullToRequest.listTop = list.getBoundingClientRect().top;
 									that._pullToRequest.touchTop = e.touches[0].clientY;
 
 									that._pullToRequest.loadingBox = ptrLoadingBox;
-									that._pullToRequest.loadingBox.classList.remove('closeAnim');
 
-
+									if(that._pullToRequest.loadingBox.classList.contains('closeAnim')){
+										that._pullToRequest.loadingBox.classList.remove('closeAnim');	
+									}									
 								}
 							});
 
@@ -910,10 +914,19 @@ function AppView(name){
 
 									var ptrBoxHeight = e.touches[0].clientY-that._pullToRequest.touchTop;
 
-									if( ptrBoxHeight <= that._pullToRequest.PTR_MAX_BOX_HEIGHT ){
-										that._pullToRequest.loadingBox.style.height = ptrBoxHeight+'px';
+									that._pullToRequest.loadingBox.style.height = ptrBoxHeight+'px';
+
+									if( ptrBoxHeight <= that._pullToRequest.PTR_MAX_BOX_HEIGHT ){										
+
+										if(that._pullToRequest.loadingBox.classList.contains('refresh')){
+											that._pullToRequest.loadingBox.classList.remove('refresh');
+										}
 									}else{
-										that._pullToRequest.loadingBox.style.height = that._pullToRequest.PTR_MAX_BOX_HEIGHT+'px';										
+										//that._pullToRequest.loadingBox.style.height = that._pullToRequest.PTR_MAX_BOX_HEIGHT+'px';
+
+										if(!that._pullToRequest.loadingBox.classList.contains('refresh')){
+											that._pullToRequest.loadingBox.classList.add('refresh');
+										}
 									}
 
 									that._pullToRequest.loadingBox.style.lineHeight = that._pullToRequest.loadingBox.style.height;
@@ -924,9 +937,13 @@ function AppView(name){
 							list.addEventListener("touchend", function(e){
 								if( that._pullToRequest.loadingBox ){
 
+									if(that._pullToRequest.loadingBox.classList.contains('refresh')){
+										that._pullToRequest.loadingBox.classList.remove('refresh');
+									}
+
 									that._pullToRequest.loadingBox.classList.add('closeAnim');
 									that._pullToRequest.loadingBox.style.height = '';
-									that._pullToRequest.loadingBox = null;									
+									that._pullToRequest.loadingBox = null;
 								}
 							});
 						}
